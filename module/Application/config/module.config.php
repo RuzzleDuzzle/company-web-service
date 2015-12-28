5784051ab54405d4e3daf5e4e3d8317157a23769
RuzzleDuzzle <ruzzleduzzle18@gmail.com>
2015-12-28 18:00:54 +0200

    Minor change.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -72,18 +72,14 @@ return array(
         'doctype' => 'HTML5',
         'not_found_template' => 'error/404',
         'exception_template' => 'error/index',
-        'under_maintenance_template' => 'error/under-maintenance',
         'template_map' => array(
             'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
             'error/404' => __DIR__ . '/../view/error/404.phtml',
-            'error/403' => __DIR__ . '/../view/error/403.phtml',
-            'error/index' => __DIR__ . '/../view/error/index.phtml',
-            'error/under-maintenance' => __DIR__ . '/../view/error/under-maintenance.phtml',
+            'error/index' => __DIR__ . '/../view/error/index.phtml'
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
         ),
-        'layout' => 'layout/not-authorized',
         'strategies' => array(
             'ViewJsonStrategy',
         ),
